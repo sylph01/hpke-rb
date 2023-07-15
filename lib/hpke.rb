e2cd7ed0b73345c689d827c6d90492c41cb9ecd9
Ryo Kajiwara <sylph01@s01.ninja>
2023-07-15 19:01:21 +0900

    Initial commit after `bundle gem`

---
+++ lib/hpke.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "hpke/version"
+
+module Hpke
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/hpke/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Hpke
+  VERSION = "0.1.0"
+end
